503da7faf5d75c7bcbc01e1c5f8d414a4d015a69
Andrew D.Laptev <a.d.laptev@gmail.com>
2021-02-25 00:54:24 +0300

    Debug logger for graphql requests and responses

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import config from 'config'
 import morgan from 'morgan'
+import debug from 'debug'
 import { graphqlHTTP } from 'express-graphql'
+import { print } from 'graphql'
 import schema from './schema'
 
 const app = express()
@@ -13,10 +15,19 @@ app.use(
   graphqlHTTP({
     schema,
     graphiql: true,
+    extensions: (info) => {
+      // Logger for graphql requests and responses
+      debug('sewan:graphql')(
+        'Request:\n%s\nResponse:\n%s',
+        print(info.document),
+        JSON.stringify(info.result, null, '  '),
+      )
+      return undefined
+    },
   }),
 )
 export const server = app.listen(port, () =>
-  console.log(
+  debug('sewan:server')(
     `Running a GraphQL API server at http://localhost:${port}/graphql`,
   ),
 )
